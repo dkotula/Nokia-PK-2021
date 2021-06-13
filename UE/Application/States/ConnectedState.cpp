9925e22e2595a623da0c1c1e0d22da490f897bdd
danieldabrowski97 <ddabrowski@vp.pl>
2021-06-13 20:53:50 +0200

    int with phones

--- UE/Application/States/ConnectedState.cpp
+++ UE/Application/States/ConnectedState.cpp
@@ -86,12 +86,14 @@ void ConnectedState::handleUnknownRecipient(const common::PhoneNumber from)
 {
     if (context.user.isTalking()) {
         context.timer.stopTimer();
-        context.user.setUnknownRecipientTalking();
+        context.timer.startTimer(1s);
+        context.user.showPhonesAreNotPeered(participant);
     }
     else {
         //context.db.setUnknownRecipient(from);
     }
 }
+
 
 void ConnectedState::handleTimeout()
 {
