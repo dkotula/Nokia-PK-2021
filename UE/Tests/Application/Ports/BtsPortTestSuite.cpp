ab4ed686b188d7445f0bae111bc46b4ccbd54f99
dkotula <dkotula.wieliczka@gmail.com>
2021-06-14 20:13:23 +0200

    Added shallSendCallRequest test

--- UE/Tests/Application/Ports/BtsPortTestSuite.cpp
+++ UE/Tests/Application/Ports/BtsPortTestSuite.cpp
@@ -115,4 +115,15 @@ TEST_F(BtsPortTestSuite, shallSendMessage)
     ASSERT_NO_THROW(EXPECT_EQ(PHONE_NUMBER_RECIPIENT, reader.readPhoneNumber()));
     ASSERT_NO_THROW(EXPECT_EQ("Wiadomość", reader.readRemainingText()));
 }
+
+TEST_F(BtsPortTestSuite, shallSendCallRequest)
+{
+    common::BinaryMessage msg;
+    EXPECT_CALL(transportMock, sendMessage(_)).WillOnce([&msg](auto param) { msg = std::move(param); return true; });
+    objectUnderTest.sendCallRequest(PHONE_NUMBER_RECIPIENT);
+    common::IncomingMessage reader(msg);
+    ASSERT_NO_THROW(EXPECT_EQ(common::MessageId::CallRequest, reader.readMessageId()) );
+    ASSERT_NO_THROW(EXPECT_EQ(PHONE_NUMBER, reader.readPhoneNumber()));
+    ASSERT_NO_THROW(EXPECT_EQ(PHONE_NUMBER_RECIPIENT, reader.readPhoneNumber()));
+}
 }
